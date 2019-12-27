384679b1e4f356631c7941dd19320ec3c860b5df
Pablo M <pazabalm@gmail.com>
2019-12-27 16:04:42 -0300

    cambio sobre json

--- 04-noticias/src/app/interfaces/interfaces.ts
+++ 04-noticias/src/app/interfaces/interfaces.ts
@@ -1,23 +1,42 @@
 // se coloca export adelante para utilizar en otros métodos
 
-export interface RespuestaTopHeadlines {
-  status: string;
-  totalResults: number;
-  articles: Article[];
+
+/*
+  export interface RespuestaTopHeadlines {
+    version: string;
+    user_comment: string;
+    home_page_url: string;
+    feed_url: string;
+    title: string;
+    description: string
+    articles: Article[];
+  }
+*/
+
+  export interface Author {
+    name: string;
+    url: string;
+    avatar: string;
 }
 
-export interface Article {
-  source: Source;
-  author?: string;
-  title: string;
-  description?: string;
-  url: string;
-  urlToImage?: string;
-  publishedAt: string;
-  content?: string;
+export interface Item {
+    id: string;
+    url: string;
+    title: string;
+    content_html: string;
+    content_text: string;
+    date_published: Date;
+    date_modified: Date;
+    author: Author;
+    image: string;
 }
 
-export interface Source {
-  id?: string;
-  name: string;
+export interface RootObject {
+    version: string;
+    user_comment: string;
+    home_page_url: string;
+    feed_url: string;
+    title: string;
+    description: string;
+    items: Item[];
 }
--- 04-noticias/src/app/pages/tab1/tab1.page.ts
+++ 04-noticias/src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'; // faltaba el OnInit
 import { NoticiasService } from 'src/app/services/noticias.service';
-import { Article } from 'src/app/interfaces/interfaces';
+import { Item } from 'src/app/interfaces/interfaces';
 
 
 @Component({
@@ -14,7 +14,7 @@ export class Tab1Page implements OnInit {
   constructor(private noticiasService: NoticiasService) {
   }
 
-  noticias: Article[] = []; // aquí se guardarán las noticias
+  noticias: Item[] = []; // aquí se guardarán las noticias
 
   ngOnInit() {
 
@@ -25,7 +25,7 @@ export class Tab1Page implements OnInit {
     */
         this.noticiasService.getTopHeadLines()
         .subscribe( resp => {console.log('noticias',resp);  
-        this.noticias.push( ...resp.articles ) // Con los ... se añaden al arreglo articulo por artículo de forma independiente
+        this.noticias.push( ...resp.items) // Con los ... se añaden al arreglo articulo por artículo de forma independiente
          })
 
   }
